ef1dbc260ca50b4a027189270fda534791ee25c5
keyeleo <keyeleo@163.com>
2016-06-30 11:06:51 +0800

    libkeye, server in one directory

---
+++ libkeye/stdafx.h
@@ -0,0 +1,6 @@
+#ifndef _stdafx_h_
+#define _stdafx_h_
+
+#include "keye/keye_pch.hpp"
+
+#endif // _stdafx_h_
---
+++ server/test_server.cpp
@@ -0,0 +1,20 @@
+// test_server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <keye/htio/htio_demo.h>
+
+#ifdef WIN32
+#include <conio.h>
+#else
+#include <curses.h>
+#endif
+
+using namespace keye;
+
+int main(int argc, char* argv[]) {
+	unsigned short port = 8899;
+	myserver(port, 4, 4);
+
+	return 0;
+}
