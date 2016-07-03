34ed3b3f32c36ac339b0d36a67c32b53fc8d4836
vliu <vic@happylatte.com>
2016-07-03 13:04:56 +0800

    server.sh

---
+++ server.sh
@@ -0,0 +1,14 @@
+vm=vic
+
+shopt -s expand_aliases
+alias doma='docker-machine'
+
+doma start $vm
+eval "$(doma env $vm)"
+
+# run this at the first time
+#docker run -v `pwd`:/var/data --name=data_volume docker_gcc
+docker run -it --volumes-from=data_volume ubuntu:16.04
+echo export DEPENDENCIES=/var/data/sdks
+echo then run make
+
