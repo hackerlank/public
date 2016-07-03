d70c745732a30a5088276513b49f873be792f8a6
vliu <vic@happylatte.com>
2016-07-03 22:38:14 +0800

    shell

--- client.sh
+++ client.sh
@@ -8,5 +8,5 @@ eval "$(doma env $vm)"
 
 # run this at the first time
 #docker run -v `pwd`:/var/data --name=data_volume docker_gcc
-docker run -it --volumes-from=data_volume ubuntu:16.04 /var/data/server/server
+docker run -it --volumes-from=data_volume ubuntu:16.04 /var/data/client/client
 
