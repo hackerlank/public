7d5fdd10c4c9a011b23b5bd94e5bce57da5b0ec4
keyeleo <keyeleo@163.com>
2016-07-04 13:44:36 +0800

    from Windows

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 default all:
 	make -C libkeye
 	make -C server
+	make -C client
 
 clean:
 	make -C libkeye clean
 	make -C server clean
+	make -C client clean
 
 echo:
 	make -C libkeye echo
 	make -C server echo
+	make -C client echo
 
 .PHONY: default all
--- server.sh
+++ server.sh
@@ -8,5 +8,5 @@ eval "$(doma env $vm)"
 
 # run this at the first time
 #docker run -v `pwd`:/var/data --name=data_volume docker_gcc
-docker run -it --net=host --volumes-from=data_volume ubuntu:16.04 /var/data/server/server
+docker run -i --net=host --volumes-from=data_volume ubuntu:16.04 /var/data/server/server
 
