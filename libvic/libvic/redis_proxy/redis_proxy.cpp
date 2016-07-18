4b84f0e171b1589286104546daa5d3623fd9e49e
keyeleo <keyeleo@163.com>
2016-07-18 13:31:25 +0800

    redis r3c proxy

--- libvic/libvic/redis_proxy/redis_proxy.cpp
+++ libvic/libvic/redis_proxy/redis_proxy.cpp
@@ -32,6 +32,30 @@ void copy(result_t& dest,const redisReply& src){
 #define CHECK_BOOL	CHECK(false)
 #define CHECK_INT	CHECK(-1)
 
+#define DO_RET_REF_ARG1(CMD,RETURN,arg0,arg1) \
+	if(!redis){ \
+		KEYE_LOG("redis not connected\n"); \
+		return RETURN; \
+	} \
+	try{ \
+		return redis->CMD(key,arg0,&arg1); \
+	} catch(r3c::CRedisException& ex){ \
+		KEYE_LOG("redis_proxy::"#CMD" ERROR: %s",ex.str().c_str()); \
+		return RETURN; \
+	}
+
+#define DO_RET_ARG1(CMD,RETURN,arg0,arg1) \
+	if(!redis){ \
+		KEYE_LOG("redis not connected\n"); \
+		return RETURN; \
+	} \
+	try{ \
+		return redis->CMD(key,arg0,arg1); \
+	} catch(r3c::CRedisException& ex){ \
+		KEYE_LOG("redis_proxy::"#CMD" ERROR: %s",ex.str().c_str()); \
+		return RETURN; \
+	}
+
 #define DO_RET_ARG0(CMD,RETURN,arg0) \
 	if(!redis){ \
 		KEYE_LOG("redis not connected\n"); \
@@ -39,6 +63,18 @@ void copy(result_t& dest,const redisReply& src){
 	} \
 	try{ \
 		return redis->CMD(key,arg0); \
+	} catch(r3c::CRedisException& ex){ \
+		KEYE_LOG("redis_proxy::"#CMD" ERROR: %s",ex.str().c_str()); \
+		return RETURN; \
+	}
+
+#define DO_RET_REF_ARG0(CMD,RETURN,arg0) \
+	if(!redis){ \
+		KEYE_LOG("redis not connected\n"); \
+		return RETURN; \
+	} \
+	try{ \
+		return redis->CMD(key,&arg0); \
 	} catch(r3c::CRedisException& ex){ \
 		KEYE_LOG("redis_proxy::"#CMD" ERROR: %s",ex.str().c_str()); \
 		return RETURN; \
@@ -126,48 +162,182 @@ void redis_proxy::set(const char* key,const char* value){
 	DO_ARG0(set,value)
 }
 
-bool redis_proxy::setnx(const char* key,const char* value){return 0;}	//set if not exists
-bool redis_proxy::get(const char* key,std::string& value){return 0;}
+bool redis_proxy::setnx(const char* key,const char* value){
+	DO_RET_ARG0(setnx,false,value)
+}	//set if not exists
+
+bool redis_proxy::get(const char* key,std::string& value){
+	DO_RET_REF_ARG0(get,false,value)
+}
 
 // list
-int redis_proxy::llen(const char* key){return 0;}
-bool redis_proxy::lpop(const char* key,std::string& value){return 0;}		//pop from front
-int redis_proxy::lpush(const char* key,const std::vector<std::string>& values){return 0;}
-int redis_proxy::lpushx(const char* key,const std::vector<std::string>& values){return 0;}				//push if list exists
-int redis_proxy::lrange(const char* key,int start,int end,std::vector<std::string>& values){return 0;}	//get range elements
-bool redis_proxy::ltrim(const char* key,int start,int end){return 0;}		//remove range elements
-bool redis_proxy::rpop(const char* key,std::string& value){return 0;}		//pop from back
-int redis_proxy::rpush(const char* key,const char* value){return 0;}
-int redis_proxy::rpush(const char* key,const std::vector<std::string>& values){return 0;}
-int redis_proxy::rpushx(const char* key,const char* value){return 0;}
+int redis_proxy::llen(const char* key){
+	DO_RET(llen,-1)
+}
+
+bool redis_proxy::lpop(const char* key,std::string& value){
+	DO_RET_REF_ARG0(lpop,false,value)
+}		//pop from front
+
+int redis_proxy::lpush(const char* key,const std::vector<std::string>& values){
+	DO_RET_ARG0(lpush,-1,values)
+}
+
+int redis_proxy::lpushx(const char* key,const std::vector<std::string>& values){
+	//DO_RET_ARG0(lpushx,-1,values)
+	//not implemented yet
+	return -1;
+}				//push if list exists
+
+int redis_proxy::lrange(const char* key,int start,int end,std::vector<std::string>& values){
+	CHECK_INT
+	try{
+		return redis->lrange(key,start,end,&values);
+	} catch(r3c::CRedisException& ex){
+		KEYE_LOG("redis_proxy::lrange ERROR: %s",ex.str().c_str());
+		return -1;
+	}
+}	//get range elements
+
+bool redis_proxy::ltrim(const char* key,int start,int end){
+	CHECK_BOOL
+	try{
+		return redis->ltrim(key,start,end);
+	} catch(r3c::CRedisException& ex){
+		KEYE_LOG("redis_proxy::ltrim ERROR: %s",ex.str().c_str());
+		return -1;
+	}
+}		//remove range elements
+
+bool redis_proxy::rpop(const char* key,std::string& value){
+	DO_RET_REF_ARG0(rpop,false,value)
+}		//pop from back
+
+int redis_proxy::rpush(const char* key,const char* value){
+	DO_RET_ARG0(rpush,-1,value)
+}
+
+int redis_proxy::rpush(const char* key,const std::vector<std::string>& values){
+	DO_RET_ARG0(rpush,-1,values)
+}
+
+int redis_proxy::rpushx(const char* key,const char* value){
+	DO_RET_ARG0(rpushx,-1,value)
+}
 
 // hash
-int redis_proxy::hlen(const char* key){return 0;}
-int redis_proxy::hdel(const char* key,const std::vector<std::string>& fields){return 0;}
-bool redis_proxy::hexists(const char* key,const char* field){return 0;}						//if exists field
-bool redis_proxy::hset(const char* key,const char* field,const char* value){return 0;}
-bool redis_proxy::hsetnx(const char* key,const char* field,const char* value){return 0;}
-bool redis_proxy::hget(const char* key,const char* field,std::string& value){return 0;}
-void redis_proxy::hmset(const char* key,const std::map<std::string,std::string>& map){}	//set multiple values
-int redis_proxy::hmget(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map){return 0;}
-int redis_proxy::hgetall(const char* key,std::map<std::string,std::string>& map){return 0;}		//get all
-int redis_proxy::hkeys(const char* key,std::vector<std::string>& fields){return 0;}				//get all keys
-int redis_proxy::hvals(const char* key,std::vector<std::string>& vals){return 0;}				//get all values
+int redis_proxy::hlen(const char* key){
+	DO_RET(hlen,-1)
+}
+
+int redis_proxy::hdel(const char* key,const std::vector<std::string>& fields){
+	DO_RET_ARG0(hdel,-1,fields)
+}
+
+bool redis_proxy::hexists(const char* key,const char* field){
+	DO_RET_ARG0(hexists,false,field)
+}						//if exists field
+
+bool redis_proxy::hset(const char* key,const char* field,const char* value){
+	DO_RET_ARG1(hset,false,field,value)
+}
+
+bool redis_proxy::hsetnx(const char* key,const char* field,const char* value){
+	DO_RET_ARG1(hsetnx,false,field,value)
+}
+
+bool redis_proxy::hget(const char* key,const char* field,std::string& value){
+	DO_RET_ARG1(hget,false,field,&value)
+}
+
+void redis_proxy::hmset(const char* key,const std::map<std::string,std::string>& map){
+	DO_ARG0(hmset,map)
+}	//set multiple values
+
+int redis_proxy::hmget(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map){
+	DO_RET_ARG1(hmget,false,fields,&map)
+}
+
+int redis_proxy::hgetall(const char* key,std::map<std::string,std::string>& map){
+	DO_RET_REF_ARG0(hgetall,-1,map)
+}		//get all
+
+int redis_proxy::hkeys(const char* key,std::vector<std::string>& fields){
+	DO_RET_REF_ARG0(hkeys,-1,fields)
+}				//get all keys
+
+int redis_proxy::hvals(const char* key,std::vector<std::string>& vals){
+	DO_RET_REF_ARG0(hvals,-1,vals)
+}				//get all values
 
 // set
-int redis_proxy::sadd(const char* key,const std::vector<std::string>& values){return 0;}
-int redis_proxy::scard(const char* key){return 0;}	//length
-bool redis_proxy::sismember(const char* key,const std::string& value){return 0;}
-int redis_proxy::smembers(const char* key,std::vector<std::string>& values){return 0;}
-int redis_proxy::spop(const char* key,int count,std::vector<std::string>& values){return 0;}
-int redis_proxy::srem(const char* key,const std::vector<std::string>& values){return 0;}		//remove
+int redis_proxy::sadd(const char* key,const std::vector<std::string>& values){
+	DO_RET_ARG0(sadd,-1,values)
+}
+
+int redis_proxy::scard(const char* key){
+	DO_RET(scard,-1)
+}	//length
+
+bool redis_proxy::sismember(const char* key,const std::string& value){
+	DO_RET_ARG0(sismember,false,value)
+}
+
+int redis_proxy::smembers(const char* key,std::vector<std::string>& values){
+	DO_RET_REF_ARG0(smembers,-1,values)
+}
+
+int redis_proxy::spop(const char* key,int count,std::vector<std::string>& values){
+	DO_RET_ARG1(spop,-1,count,&values)
+}
+
+int redis_proxy::srem(const char* key,const std::vector<std::string>& values){
+	DO_RET_ARG0(srem,-1,values)
+}		//remove
 
 // sort set
-int redis_proxy::zadd(const char* key,const std::map<std::string,int64_t>& map){return 0;}
-int redis_proxy::zcard(const char* key){return 0;}	//length
-int redis_proxy::zcount(const char* key,int64_t min,int64_t max){return 0;}	//count range
-int redis_proxy::zrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){return 0;}
-int redis_proxy::zrevrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){return 0;}
-int redis_proxy::zrank(const char* key,const char* field){return 0;}		//get rank
-int redis_proxy::zrevrank(const char* key,const char* field){return 0;}		//get reverse rank
-long long redis_proxy::zscore(const char* key,const char* field){return 0;}		//get value
+int redis_proxy::zadd(const char* key,const std::map<std::string,int64_t>& map){
+	DO_RET_ARG0(zadd,-1,map)
+}
+
+int redis_proxy::zcard(const char* key){
+	//DO_RET(zcard,-1)
+	//not implemented yet
+	return -1;
+}	//length
+
+int redis_proxy::zcount(const char* key,int64_t min,int64_t max){
+	DO_RET_ARG1(zcount,-1,min,max)
+}	//count range
+
+int redis_proxy::zrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){
+	CHECK_INT;
+	try{
+		return redis->zrange(key,start,end,withscores,&vec);
+	} catch(r3c::CRedisException& ex){
+		KEYE_LOG("redis_proxy::zrange ERROR: %s",ex.str().c_str());
+		return -1;
+	}
+}
+
+int redis_proxy::zrevrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){
+	CHECK_INT;
+	try{
+		return redis->zrevrange(key,start,end,withscores,&vec);
+	} catch(r3c::CRedisException& ex){
+		KEYE_LOG("redis_proxy::zrevrange ERROR: %s",ex.str().c_str());
+		return -1;
+	}
+}
+
+int redis_proxy::zrank(const char* key,const char* field){
+	DO_RET_ARG0(zrank,-1,field)
+}		//get rank
+
+int redis_proxy::zrevrank(const char* key,const char* field){
+	DO_RET_ARG0(zrevrank,-1,field)
+}		//get reverse rank
+
+long long redis_proxy::zscore(const char* key,const char* field){
+	DO_RET_ARG0(zscore,-1,field)
+}		//get value
