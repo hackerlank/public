6d878ed94a101c8364eb959bb31e82bd80fb0bcd
keyeleo <keyeleo@163.com>
2016-07-18 10:41:58 +0800

    Revert "add parameter to be compatible with mysql"
    This reverts commit c8ea698071f4957117beb499a09e3b7f99797072.

--- libvic/libvic/redis_proxy/redis_proxy.cpp
+++ libvic/libvic/redis_proxy/redis_proxy.cpp
@@ -26,54 +26,54 @@ bool redis_proxy::connect(const char* host, unsigned short port,
 int redis_proxy::command(result_t&,const char* cmd){return 0;}
 
 //key
-bool redis_proxy::exists(const char* key,const char*){return 0;}
-bool redis_proxy::expire(const char* key,uint32_t seconds,const char*){return 0;}
-bool redis_proxy::del(const char* key,const char*){return 0;}
+bool redis_proxy::exists(const char* key){return 0;}
+bool redis_proxy::expire(const char* key,uint32_t seconds){return 0;}
+bool redis_proxy::del(const char* key){return 0;}
 
 //string							
-void redis_proxy::set(const char* key,const char* value,const char*){}
-bool redis_proxy::setnx(const char* key,const char* value,const char*){return 0;}	//set if not exists
-bool redis_proxy::get(const char* key,std::string& value,const char*){return 0;}
+void redis_proxy::set(const char* key,const char* value){}
+bool redis_proxy::setnx(const char* key,const char* value){return 0;}	//set if not exists
+bool redis_proxy::get(const char* key,std::string& value){return 0;}
 
 // list
-int redis_proxy::llen(const char* key,const char*){return 0;}
-bool redis_proxy::lpop(const char* key,std::string& value,const char*){return 0;}		//pop from front
-int redis_proxy::lpush(const char* key,const std::vector<std::string>& values,const char*){return 0;}
-int redis_proxy::lpushx(const char* key,const std::vector<std::string>& values,const char*){return 0;}				//push if list exists
-int redis_proxy::lrange(const char* key,int start,int end,std::vector<std::string>& values,const char*){return 0;}	//get range elements
-bool redis_proxy::ltrim(const char* key,int start,int end,const char*){return 0;}		//remove range elements
-bool redis_proxy::rpop(const char* key,std::string& value,const char*){return 0;}		//pop from back
-int redis_proxy::rpush(const char* key,const char* value,const char*){return 0;}
-int redis_proxy::rpush(const char* key,const std::vector<std::string>& values,const char*){return 0;}
-int redis_proxy::rpushx(const char* key,const char* value,const char*){return 0;}
+int redis_proxy::llen(const char* key){return 0;}
+bool redis_proxy::lpop(const char* key,std::string& value){return 0;}		//pop from front
+int redis_proxy::lpush(const char* key,const std::vector<std::string>& values){return 0;}
+int redis_proxy::lpushx(const char* key,const std::vector<std::string>& values){return 0;}				//push if list exists
+int redis_proxy::lrange(const char* key,int start,int end,std::vector<std::string>& values){return 0;}	//get range elements
+bool redis_proxy::ltrim(const char* key,int start,int end){return 0;}		//remove range elements
+bool redis_proxy::rpop(const char* key,std::string& value){return 0;}		//pop from back
+int redis_proxy::rpush(const char* key,const char* value){return 0;}
+int redis_proxy::rpush(const char* key,const std::vector<std::string>& values){return 0;}
+int redis_proxy::rpushx(const char* key,const char* value){return 0;}
 
 // hash
-int redis_proxy::hlen(const char* key,const char*){return 0;}
-int redis_proxy::hdel(const char* key,const std::vector<std::string>& fields,const char*){return 0;}
-bool redis_proxy::hexists(const char* key,const char* field,const char*){return 0;}						//if exists field
-bool redis_proxy::hset(const char* key,const char* field,const char* value,const char*){return 0;}
-bool redis_proxy::hsetnx(const char* key,const char* field,const char* value,const char*){return 0;}
-bool redis_proxy::hget(const char* key,const char* field,std::string& value,const char*){return 0;}
-void redis_proxy::hmset(const char* key,const std::map<std::string,std::string>& map,const char*){}	//set multiple values
-int redis_proxy::hmget(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map,const char*){return 0;}
-int redis_proxy::hgetall(const char* key,std::map<std::string,std::string>& map,const char*){return 0;}		//get all
-int redis_proxy::hkeys(const char* key,std::vector<std::string>& fields,const char*){return 0;}				//get all keys
-int redis_proxy::hvals(const char* key,std::vector<std::string>& vals,const char*){return 0;}				//get all values
+int redis_proxy::hlen(const char* key){return 0;}
+int redis_proxy::hdel(const char* key,const std::vector<std::string>& fields){return 0;}
+bool redis_proxy::hexists(const char* key,const char* field){return 0;}						//if exists field
+bool redis_proxy::hset(const char* key,const char* field,const char* value){return 0;}
+bool redis_proxy::hsetnx(const char* key,const char* field,const char* value){return 0;}
+bool redis_proxy::hget(const char* key,const char* field,std::string& value){return 0;}
+void redis_proxy::hmset(const char* key,const std::map<std::string,std::string>& map){}	//set multiple values
+int redis_proxy::hmget(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map){return 0;}
+int redis_proxy::hgetall(const char* key,std::map<std::string,std::string>& map){return 0;}		//get all
+int redis_proxy::hkeys(const char* key,std::vector<std::string>& fields){return 0;}				//get all keys
+int redis_proxy::hvals(const char* key,std::vector<std::string>& vals){return 0;}				//get all values
 
 // set
-int redis_proxy::sadd(const char* key,const std::vector<std::string>& values,const char*){return 0;}
-int redis_proxy::scard(const char* key,const char*){return 0;}	//length
-bool redis_proxy::sismember(const char* key,const std::string& value,const char*){return 0;}
-int redis_proxy::smembers(const char* key,std::vector<std::string>& values,const char*){return 0;}
-int redis_proxy::spop(const char* key,int count,std::vector<std::string>& values,const char*){return 0;}
-int redis_proxy::srem(const char* key,const std::vector<std::string>& values,const char*){return 0;}		//remove
+int redis_proxy::sadd(const char* key,const std::vector<std::string>& values){return 0;}
+int redis_proxy::scard(const char* key){return 0;}	//length
+bool redis_proxy::sismember(const char* key,const std::string& value){return 0;}
+int redis_proxy::smembers(const char* key,std::vector<std::string>& values){return 0;}
+int redis_proxy::spop(const char* key,int count,std::vector<std::string>& values){return 0;}
+int redis_proxy::srem(const char* key,const std::vector<std::string>& values){return 0;}		//remove
 
 // sort set
-int redis_proxy::zadd(const char* key,const std::map<std::string,int64_t>& map,const char*){return 0;}
-int redis_proxy::zcard(const char* key,const char*){return 0;}	//length
-int redis_proxy::zcount(const char* key,int64_t min,int64_t max,const char*){return 0;}	//count range
-int redis_proxy::zrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec,const char*){return 0;}
-int redis_proxy::zrevrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec,const char*){return 0;}
-int redis_proxy::zrank(const char* key,const char* field,const char*){return 0;}		//get rank
-int redis_proxy::zrevrank(const char* key,const char* field,const char*){return 0;}		//get reverse rank
-long long redis_proxy::zscore(const char* key,const char* field,const char*){return 0;}		//get value
+int redis_proxy::zadd(const char* key,const std::map<std::string,int64_t>& map){return 0;}
+int redis_proxy::zcard(const char* key){return 0;}	//length
+int redis_proxy::zcount(const char* key,int64_t min,int64_t max){return 0;}	//count range
+int redis_proxy::zrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){return 0;}
+int redis_proxy::zrevrange(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec){return 0;}
+int redis_proxy::zrank(const char* key,const char* field){return 0;}		//get rank
+int redis_proxy::zrevrank(const char* key,const char* field){return 0;}		//get reverse rank
+long long redis_proxy::zscore(const char* key,const char* field){return 0;}		//get value
--- libvic/libvic/redis_proxy/redis_proxy.h
+++ libvic/libvic/redis_proxy/redis_proxy.h
@@ -28,57 +28,57 @@ public:
 	int			command(result_t&,const char* cmd);
 
 	//key
-	bool	exists		(const char* key,const char* table=nullptr);
-	bool	expire		(const char* key,uint32_t seconds,const char* table=nullptr);
-	bool	del			(const char* key,const char* table=nullptr);
+	bool	exists		(const char* key);
+	bool	expire		(const char* key,uint32_t seconds);
+	bool	del			(const char* key);
 
 	//string
-	void	set			(const char* key,const char* value,const char* table=nullptr);
-	bool	setnx		(const char* key,const char* value,const char* table=nullptr);		//set if not exists
-	bool	get			(const char* key,std::string& value,const char* table=nullptr);
+	void	set			(const char* key,const char* value);
+	bool	setnx		(const char* key,const char* value);	//set if not exists
+	bool	get			(const char* key,std::string& value);
 
 	// list
-	int		llen		(const char* key,const char* table=nullptr);
-	bool	lpop		(const char* key,std::string& value,const char* table=nullptr);		//pop from front
-	int		lpush		(const char* key,const std::vector<std::string>& values,const char* table=nullptr);
-	int		lpushx		(const char* key,const std::vector<std::string>& values,const char* table=nullptr);				//push if list exists
-	int		lrange		(const char* key,int start,int end,std::vector<std::string>& values,const char* table=nullptr);	//get range elements
-	bool	ltrim		(const char* key,int start,int end,const char* table=nullptr);		//remove range elements
-	bool	rpop		(const char* key,std::string& value,const char* table=nullptr);		//pop from back
-	int		rpush		(const char* key,const char* value,const char* table=nullptr);
-	int		rpush		(const char* key,const std::vector<std::string>& values,const char* table=nullptr);
-	int		rpushx		(const char* key,const char* value,const char* table=nullptr);
+	int		llen		(const char* key);
+	bool	lpop		(const char* key,std::string& value);		//pop from front
+	int		lpush		(const char* key,const std::vector<std::string>& values);
+	int		lpushx		(const char* key,const std::vector<std::string>& values);				//push if list exists
+	int		lrange		(const char* key,int start,int end,std::vector<std::string>& values);	//get range elements
+	bool	ltrim		(const char* key,int start,int end);		//remove range elements
+	bool	rpop		(const char* key,std::string& value);		//pop from back
+	int		rpush		(const char* key,const char* value);
+	int		rpush		(const char* key,const std::vector<std::string>& values);
+	int		rpushx		(const char* key,const char* value);
 
 	// hash
-	int		hlen		(const char* key,const char* table=nullptr);
-	int		hdel		(const char* key,const std::vector<std::string>& fields,const char* table=nullptr);
-	bool	hexists		(const char* key,const char* field,const char* table=nullptr);								//if exists field
-	bool	hset		(const char* key,const char* field,const char* value,const char* table=nullptr);
-	bool	hsetnx		(const char* key,const char* field,const char* value,const char* table=nullptr);
-	bool	hget		(const char* key,const char* field,std::string& value,const char* table=nullptr);
-	void	hmset		(const char* key,const std::map<std::string,std::string>& map,const char* table=nullptr);	//set multiple values
-	int		hmget		(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map,const char* table=nullptr);
-	int		hgetall		(const char* key,std::map<std::string,std::string>& map,const char* table=nullptr);			//get all
-	int		hkeys		(const char* key,std::vector<std::string>& fields,const char* table=nullptr);				//get all keys
-	int		hvals		(const char* key,std::vector<std::string>& vals,const char* table=nullptr);					//get all values
+	int		hlen		(const char* key);
+	int		hdel		(const char* key,const std::vector<std::string>& fields);
+	bool	hexists		(const char* key,const char* field);						//if exists field
+	bool	hset		(const char* key,const char* field,const char* value);
+	bool	hsetnx		(const char* key,const char* field,const char* value);
+	bool	hget		(const char* key,const char* field,std::string& value);
+	void	hmset		(const char* key,const std::map<std::string,std::string>& map);	//set multiple values
+	int		hmget		(const char* key,const std::vector<std::string>& fields,std::map<std::string,std::string>& map);
+	int		hgetall		(const char* key,std::map<std::string,std::string>& map);		//get all
+	int		hkeys		(const char* key,std::vector<std::string>& fields);				//get all keys
+	int		hvals		(const char* key,std::vector<std::string>& vals);				//get all values
 
 	// set
-	int		sadd		(const char* key,const std::vector<std::string>& values,const char* table=nullptr);
-	int		scard		(const char* key,const char* table=nullptr);	//length
-	bool	sismember	(const char* key,const std::string& value,const char* table=nullptr);
-	int		smembers	(const char* key,std::vector<std::string>& values,const char* table=nullptr);
-	int		spop		(const char* key,int count,std::vector<std::string>& values,const char* table=nullptr);
-	int		srem		(const char* key,const std::vector<std::string>& values,const char* table=nullptr);			//remove
+	int		sadd		(const char* key,const std::vector<std::string>& values);
+	int		scard		(const char* key);	//length
+	bool	sismember	(const char* key,const std::string& value);
+	int		smembers	(const char* key,std::vector<std::string>& values);
+	int		spop		(const char* key,int count,std::vector<std::string>& values);
+	int		srem		(const char* key,const std::vector<std::string>& values);		//remove
 
 	// sort set
-	int		zadd		(const char* key,const std::map<std::string,int64_t>& map,const char* table=nullptr);
-	int		zcard		(const char* key,const char* table=nullptr);	//length
-	int		zcount		(const char* key,int64_t min,int64_t max,const char* table=nullptr);	//count range
-	int		zrange		(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec,const char* table=nullptr);
-	int		zrevrange	(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec,const char* table=nullptr);
-	int		zrank		(const char* key,const char* field,const char* table=nullptr);			//get rank
-	int		zrevrank	(const char* key,const char* field,const char* table=nullptr);			//get reverse rank
-	long long zscore	(const char* key,const char* field,const char* table=nullptr);			//get value
+	int		zadd		(const char* key,const std::map<std::string,int64_t>& map);
+	int		zcard		(const char* key);	//length
+	int		zcount		(const char* key,int64_t min,int64_t max);	//count range
+	int		zrange		(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec);
+	int		zrevrange	(const char* key,int start,int end,bool withscores,std::vector<std::pair<std::string,int64_t>>& vec);
+	int		zrank		(const char* key,const char* field);		//get rank
+	int		zrevrank	(const char* key,const char* field);		//get reverse rank
+	long long zscore	(const char* key,const char* field);		//get value
 protected:
 	std::shared_ptr<r3c::CRedisClient>	redis;
 };
