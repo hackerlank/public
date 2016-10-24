0e4834b0ee0b81eb4ec720d6996626baf6727762
vliu <vic@happylatte.com>
2016-10-24 22:10:44 +0800

    tweaked settle popup;

---
+++ ImmortalCard/Assets/Scripts/Prefabs/ZipaiBunch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Proto3;
+
+public class ZipaiBunch : MonoBehaviour {
+	public Text		Type;
+	public Text		Score;
+	public Zipai[]	Cards;
+
+	public bunch_t Value{
+		set{
+
+		}
+	}
+}
