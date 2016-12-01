8b53a45847efa1430bd2cd630728fe693466293e
vliu <vic@happylatte.com>
2016-12-01 21:20:05 +0800

    update;

--- ImmortalCard/Assets/Scripts/OtherPanel/LoadingPanel.cs
+++ ImmortalCard/Assets/Scripts/OtherPanel/LoadingPanel.cs
@@ -24,12 +24,10 @@ public class LoadingPanel : MonoBehaviour {
 			Instance=null;
 	}
 
-
 	public IEnumerator Process(){
-		children.SetActive(true);
-#if DEVELOPMENT_BUILD
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
 		Host.gameObject.SetActive(true);
-		BtnLogin.SetActive(true);
+		Login.SetActive(true);
 #endif
 		//load host from cache
 		DefaultHost.text=Config.ws;
@@ -51,11 +49,7 @@ public class LoadingPanel : MonoBehaviour {
 	}
 
 	IEnumerator updateCo(){
-		fileProgress.value = 0;
-		
-		//foreach(object o in Game.ToEnumerable(Upgrade()))yield return o;
-		//if(Configs.Testing.skipResourceUpdated>0)yield break;
-		
+		//restart download manager
 		if(DownloadManager.Instance!=null)
 			DestroyImmediate(DownloadManager.Instance.gameObject);
 		yield return null;
@@ -65,7 +59,8 @@ public class LoadingPanel : MonoBehaviour {
 		}
 		
 		while(!DownloadManager.Instance.ConfigLoaded)yield return null;
-		
+
+		//prepare download list via priority
 		var bundleInProgress=new List<string>();
 		var bundleProgress=new List<string>();
 		var lowPriority=new List<string>();
@@ -80,44 +75,48 @@ public class LoadingPanel : MonoBehaviour {
 			}else
 				lowPriority.Add(url);
 		}
-		
-		int totalInProgress=bundleInProgress.Count;
-		if(bundleInProgress.Count+lowPriority.Count>0){
-			Debug.Log("need download");
-		}
-		
+
+		//nothing to do
+		if(bundleInProgress.Count+lowPriority.Count<=0)
+			yield break;
+
+		//wait for device caching ready
 		var tm=Time.realtimeSinceStartup;
 		while(!Caching.ready&&Time.realtimeSinceStartup-tm<5f)yield return null;
+		if(!Caching.ready)
+			Debug.LogError("device storage full or not writalble");
 		
-		foreach(string url in bundleInProgress)DownloadManager.Instance.StartDownload (url,9);
-		
-		if(bundleProgress.Count>0)state=progressString(0,totalInProgress,totalInProgress);
-		
-		while(bundleInProgress.Count>0){
-			fileProgress.value = DownloadManager.Instance.ProgressOfBundles (bundleProgress.ToArray ());
-			foreach(string url in bundleInProgress){
-				var www=DownloadManager.Instance.GetWWW(url);
-				if(www!=null&&www.isDone){
-					string resname=Updater.MakeName(url);
-					Main.Instance.updater.AddResource(resname,www);
-					bundleInProgress.Remove(url);
-					state=progressString(fileProgress.value,bundleInProgress.Count,totalInProgress);
-					Debug.Log("----Asset updated "+resname);
-					break;
+		if(bundleProgress.Count>0){
+			children.SetActive(true);
+			setProgress(0,bundleProgress.Count,bundleProgress.Count);
+
+			//download
+			foreach(string url in bundleInProgress)DownloadManager.Instance.StartDownload (url,9);
+			while(bundleInProgress.Count>0){
+				var percentage=DownloadManager.Instance.ProgressOfBundles(bundleProgress.ToArray());
+				foreach(string url in bundleInProgress){
+					var www=DownloadManager.Instance.GetWWW(url);
+					if(www!=null&&www.isDone){
+						string resname=Updater.MakeName(url);
+						Main.Instance.updater.AddResource(resname,www);
+						bundleInProgress.Remove(url);
+						setProgress(percentage,bundleInProgress.Count,bundleProgress.Count);
+						Debug.Log("----Asset updated "+resname);
+						break;
+					}
 				}
+				setProgress(percentage,bundleInProgress.Count,bundleProgress.Count);
+				yield return null;
 			}
-			state=progressString(fileProgress.value,bundleInProgress.Count,totalInProgress);
-			yield return null;
 		}
-		
-		state = "";
-		
+
+		//background download
 		foreach(string url in lowPriority)
 			DownloadManager.Instance.StartDownload (url);
 	}
 
 	IEnumerator loginCo(){
-#if DEVELOPMENT_BUILD
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
 		//choice default if empty
 		if(Host.text.Length<=0)Host.text=DefaultHost.text;
 		//caching
@@ -170,17 +169,12 @@ public class LoadingPanel : MonoBehaviour {
 		yield break;
 	}
 
-	string state{
-		set{
-
-		}
-	}
-	string progressString(float percent,int bundleInProgress,int totalInProgress){
-		var percentage=string.Format("{0:#0.}%",100.0f*percent);
-		var str=string.Format("下载...{0}",percentage);
+	void setProgress(float percent,int bundleInProgress,int totalInProgress){
+		var str=string.Format("{0:#0.}%",100.0f*percent);
 		if(Debug.isDebugBuild)str+=string.Format(" ({0}/{1})",totalInProgress-bundleInProgress,totalInProgress);
-		//Debug.Log("===="+str);
-		return str;
+		fileProgress.value = (float)(totalInProgress-bundleInProgress)/(float)totalInProgress;
+		totalProgress.value = percent;
+		percentage.text=string.Format("{0:#0.}%",100.0f*percent);
 	}
 
 	public void OnLogin(){
