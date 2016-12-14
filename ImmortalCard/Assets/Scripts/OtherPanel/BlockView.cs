68d7bc373221f24553025c2c6f4c54579a2c1e56
vliu <vic@happylatte.com>
2016-12-14 09:08:15 +0800

    add BlockView;

---
+++ ImmortalCard/Assets/Scripts/OtherPanel/BlockView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlockView : MonoBehaviour {
+	public GameObject	Spinner;
+	public Text			Info;
+
+	public Text			DlgTitle;
+	public Text			DlgContent;
+	public GameObject	DlgOk;
+	public GameObject	DlgCancel;
+
+	public void OnOk(){
+
+	}
+
+	public void OnCancel(){
+		
+	}
+}
