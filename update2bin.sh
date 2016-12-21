28c640ff0ebdfa3b7b3dc29dcf9eaef9b8e251cf
vliu <vic@happylatte.com>
2016-12-21 20:58:23 +0800

    build 103; new assets bundle; script to copy compiled files;

---
+++ update2bin.sh
@@ -0,0 +1,7 @@
+cp -rf sdks/libs/centos6/libkeye.so bin/
+cp -rf sdks/libs/centos6/libvic.so bin/
+cp -rf sdks/libs/centos6/libimmortal.so bin/
+cp -rf sdks/libs/centos6/libprotocol.so bin/
+cp -rf Node/Node bin/IMNode
+cp -rf Lobby/Lobby bin/IMLobby
+cp -rf Charge/Charge bin/IMCharge
---
+++ update2sdks.sh
@@ -0,0 +1,4 @@
+cp -rf libkeye/libkeye.so sdks/libs/centos6/
+cp -rf libvic/libvic.so sdks/libs/centos6/
+cp -rf libimmortal/libimmortal.so sdks/libs/centos6/
+cp -rf protocol/libprotocol.so sdks/libs/centos6/
